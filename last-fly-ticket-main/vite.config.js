34775f73d6a1efb92ab1a26c218ff9598023e575
projecto46 <projecto46@protonmail.com>
2025-08-24 13:01:04 +0300

    כןס פםרא

--- last-fly-ticket-main/vite.config.js
+++ last-fly-ticket-main/vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-
-
-
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
-})
+server: {
+  // אתה יכול לבחור בין true, 'localhost' או '127.0.0.1'
+  host: true,
+  // או
+  // host: '127.0.0.1',
+}
