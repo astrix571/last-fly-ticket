788c9048b3919d82e691a16a64f3a23f9520f285
projecto46 <projecto46@protonmail.com>
2025-08-24 13:03:16 +0300

    fix port

--- last-fly-ticket-main/vite.config.js
+++ last-fly-ticket-main/vite.config.js
@@ -1,6 +1,9 @@
-server: {
-  // אתה יכול לבחור בין true, 'localhost' או '127.0.0.1'
-  host: true,
-  // או
-  // host: '127.0.0.1',
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // השורה הזו פותרת את הבעיה
+  },
+});
